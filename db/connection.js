ca119792a015c03fc333ca8f1b93cc340c1698fc
sanae <sanae.k1986@gmail.com>
2025-04-14 13:05:31 +0100

    project settings

---
+++ db/connection.js
@@ -0,0 +1,20 @@
+const { Pool } = require("pg");
+
+const ENV = process.env.NODE_ENV || "development";
+
+require("dotenv").config({
+  path: `${__dirname}/../.evn.${ENV}`,
+});
+
+if (!process.env.PGDATABASE) {
+  throw new Error("No PGDATABASE configured!");
+}
+//for production
+// const config = {};
+
+// if (ENV === "production") {
+//   config.connectionString = process.env.DATABASE_URL;
+//   config.max = 2;
+// }
+
+module.exports = new Pool();
---
+++ listen.js
@@ -0,0 +1,10 @@
+const app = require("./app");
+
+const { PORT = 9090 } = process.env;
+app.listen(PORT, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(`Lsitening at port ${PORT} hello!!`);
+  }
+});
